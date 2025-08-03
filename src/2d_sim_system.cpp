4c3786edc83a3bf22446d5aa70f0963b4c69f7db
T5AUCE <liudyl323@gmail.com>
2025-08-03 19:48:48 -0400

    Basic files made

---
+++ src/2d_sim_system.cpp
@@ -0,0 +1,12 @@
+﻿// 2d_sim_system.cpp : Defines the entry point for the application.
+//
+
+#include "2d_sim_system.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
